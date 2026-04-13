8a6547739dc8e1048440b908bef26631ca7c43ef
Ahmed564676786 <ahmedking4660@gmail.com>
2026-04-13 05:30:07 +0500

    first commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+const { Socket } = require('dgram');
+const express = require('express');
+const http  = require('http');
+const socketio = require('socket.io');
+
+const port = 3000;
+const app = express();
+
+const server = http.createServer(app);
+
+const io = socketio(server,{
+
+    cors:{
+
+        origin:["*"],
+        methods:["GET","POST"],
+        credentials:true
+    }
+});
+
+
+app.use('/',(req,res,next)=>{
+
+    console.log('Hello from the server');
+
+    res.send('Home Page')
+});
+
+
+io.on('connection',(socket) =>{
+
+    console.log('User is connected',socket.id);
+    
+});
+
+
+app.listen(port,()=>{
+
+
+    console.log('server is running');
+})
